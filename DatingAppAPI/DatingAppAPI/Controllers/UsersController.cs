789f4b34f7f500fa5e4b952bfc31075fafea1442
Kay Kim <kkim@redapt.com>
2024-02-01 09:36:04 -0800

    Upload API

---
+++ DatingAppAPI/DatingAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingAppAPI.Data;
+using DatingAppAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
