7ce3805ffdfab779914e05aed04f9f6ed26f2fae
Kay Kim <kkim@redapt.com>
2024-02-01 15:07:18 -0800

    Create Client

--- DatingAppAPI/DatingAppAPI/Program.cs
+++ DatingAppAPI/DatingAppAPI/Program.cs
@@ -10,10 +10,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder =>
+    builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200")
+    .AllowCredentials());
 
 app.UseHttpsRedirection();
 
